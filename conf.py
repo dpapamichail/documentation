1cb43e5d1e41c594f409207a73903417e91623a5
dpapamichail <dpapamichail@ges.com>
2025-10-22 13:44:34 +0100

    Set documentation version to 18.0

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = 'Odoo S.A.'
 # `version` is the version info for the project being documented, acts as replacement for |version|,
 # also used in various other places throughout the built documents.
 # `release` is the full version, including a/b/rc tags. Acts as replacement for |release|.
-version = release = '19.0'
+version = release = '18.0'
 
 # `current_branch` is the technical name of the current branch.
 # E.g., saas-15.4 -> saas-15.4; 12.0 -> 12.0, master -> master (*).
